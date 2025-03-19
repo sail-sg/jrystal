cd2332f64652d6e08027f08eddfb3f518839d5f5
jiaxi98 <jiaxi.zhao@u.nus.edu>
2025-03-19 23:50:24 +0800

    update

--- jrystal/_src/pw_test.py
+++ jrystal/_src/pw_test.py
@@ -26,7 +26,7 @@ class _TestModules(parameterized.TestCase):
 
   def setUp(self):
     self.key = jax.random.PRNGKey(123)
-    diamond_file_path = "../geometry/diamond.xyz"
+    diamond_file_path = "geometry/diamond.xyz"
     self.crystal = jr.Crystal.create_from_file(diamond_file_path)
     self.num_bands = self.crystal.num_electron
     self.key = jax.random.PRNGKey(123)
@@ -35,8 +35,8 @@ class _TestModules(parameterized.TestCase):
     self.freq_mask = jr.grid.cubic_mask([7, 8, 9])
 
   def test_wave(self):
-    pw_param = jr.pw.pw_param_init(self.key, self.num_bands, 1, self.freq_mask)
-    coeff = jr.pw.pw_coeff(pw_param, self.freq_mask)
+    pw_param = jr.pw.param_init(self.key, self.num_bands, 1, self.freq_mask)
+    coeff = jr.pw.coeff(pw_param, self.freq_mask)
     wave_grid1 = jr.pw.wave_grid(coeff, self.crystal.vol)
 
     @jr.utils.vmapstack(3)
@@ -50,21 +50,21 @@ class _TestModules(parameterized.TestCase):
     np.testing.assert_allclose(wave_grid1, wave_grid2, atol=1e-8)
 
   def test_nabla_n_grid(self):
-    pw_param = jr.pw.pw_param_init(self.key, self.num_bands, 1, self.freq_mask)
-    coeff = jr.pw.pw_coeff(pw_param, self.freq_mask)
+    pw_param = jr.pw.param_init(self.key, self.num_bands, 1, self.freq_mask)
+    coeff = jr.pw.coeff(pw_param, self.freq_mask)
     occupation = jr.occupation.gamma(
       1, self.crystal.num_electron, num_bands=self.num_bands
     )
 
     r = jnp.array([0.1, 0.05, -0.6])
-    nabla_density_r1 = jr.pw.nabla_density_grid(
+    nabla_density_r1 = jr._src.pw.nabla_density_grid(
       r, coeff, self.crystal.A, self.g_vecs, occupation
     )
 
     def nabla_n(r):
 
       def f(r):
-        return jr.pw.density_r(
+        return jr._src.pw.density_r(
           r, coeff, self.crystal.A, self.g_vecs, occupation
         )
 
--- jrystal/_src/xc.py
+++ jrystal/_src/xc.py
@@ -31,6 +31,118 @@ def lda_x_spin(n, zeta, **kwargs):
     return ex, jnp.array([vx_up, vx_dw]), None
 
 
+def lda_c_pw(n, A=0.031091, a1=0.2137, b1=7.5957, b2=3.5876, b3=1.6382, b4=0.49294, **kwargs):
+    """Perdew-Wang parametrization of the correlation functional (spin-paired).
+
+    Corresponds to the functional with the label LDA_C_PW and ID 12 in Libxc.
+
+    Reference: Phys. Rev. B 45, 13244.
+
+    Args:
+        n: Real-space electronic density.
+
+    Keyword Args:
+        A: Functional parameter.
+        a1: Functional parameter.
+        b1: Functional parameter.
+        b2: Functional parameter.
+        b3: Functional parameter.
+        b4: Functional parameter.
+        **kwargs: Throwaway arguments.
+
+    Returns:
+        PW correlation energy density and potential.
+    """
+    rs = (3 / (4 * jnp.pi * n)) ** (1 / 3)
+    rs12 = jnp.sqrt(rs)
+    rs32 = rs * rs12
+    rs2 = rs**2
+
+    om = 2 * A * (b1 * rs12 + b2 * rs + b3 * rs32 + b4 * rs2)
+    olog = jnp.log(1 + 1 / om)
+    ec = -2 * A * (1 + a1 * rs) * olog
+
+    dom = 2 * A * (0.5 * b1 * rs12 + b2 * rs + 1.5 * b3 * rs32 + 2 * b4 * rs2)
+    vc = -2 * A * (1 + 2 / 3 * a1 * rs) * olog - 2 / 3 * A * (1 + a1 * rs) * dom / (om * (om + 1))
+    return ec, jnp.array([vc]), None
+
+
+def lda_c_pw_spin(n, zeta, A=(0.031091, 0.015545, 0.016887), fzeta0=1.709921, **kwargs):
+    """Perdew-Wang parametrization of the correlation functional (spin-polarized).
+
+    Corresponds to the functional with the label LDA_C_PW and ID 12 in Libxc.
+
+    Reference: Phys. Rev. B 45, 13244.
+
+    Args:
+        n: Real-space electronic density.
+        zeta: Relative spin polarization.
+
+    Keyword Args:
+        A: Functional parameters.
+        fzeta0: Functional parameter.
+        **kwargs: Throwaway arguments.
+
+    Returns:
+        PW correlation energy density and potential.
+    """
+    a1 = (0.2137, 0.20548, 0.11125)
+    b1 = (7.5957, 14.1189, 10.357)
+    b2 = (3.5876, 6.1977, 3.6231)
+    b3 = (1.6382, 3.3662, 0.88026)
+    b4 = (0.49294, 0.62517, 0.49671)
+
+    ec0, vc0, _ = lda_c_pw(n, A[0], a1[0], b1[0], b2[0], b3[0], b4[0])  # Unpolarized
+    ec1, vc1, _ = lda_c_pw(n, A[1], a1[1], b1[1], b2[1], b3[1], b4[1])  # Polarized
+    ac, dac, _ = lda_c_pw(n, A[2], a1[2], b1[2], b2[2], b3[2], b4[2])  # Spin stiffness
+    ac = -ac  # The PW spin interpolation is parametrized with -ac instead of ac
+    vc0, vc1, dac = vc0[0], vc1[0], dac[0]  # Remove spin dimension for the correct shape
+
+    fzeta = ((1 + zeta) ** (4 / 3) + (1 - zeta) ** (4 / 3) - 2) / (2 ** (4 / 3) - 2)
+    zeta3 = zeta**3
+    zeta4 = zeta3 * zeta
+
+    ec = ec0 + ac * fzeta * (1 - zeta4) / fzeta0 + (ec1 - ec0) * fzeta * zeta4
+
+    dac = -dac  # Also change the sign for the derivative
+    dfzeta = ((1 + zeta) ** (1 / 3) - (1 - zeta) ** (1 / 3)) * 4 / (3 * (2 ** (4 / 3) - 2))
+    factor1 = vc0 + dac * fzeta * (1 - zeta4) / fzeta0 + (vc1 - vc0) * fzeta * zeta4
+    factor2 = ac / fzeta0 * (dfzeta * (1 - zeta4) - 4 * fzeta * zeta3) + (ec1 - ec0) * (
+        dfzeta * zeta4 + 4 * fzeta * zeta3
+    )
+
+    vc_up = factor1 + factor2 * (1 - zeta)
+    vc_dw = factor1 - factor2 * (1 + zeta)
+    return ec, jnp.array([vc_up, vc_dw]), None
+
+
+def lda_c_pw_mod_spin(n, zeta, **kwargs):
+    """Modified Perdew-Wang parametrization of the correlation functional (spin-polarized).
+
+    Corresponds to the functional with the label LDA_C_PW_MOD and ID 13 in Libxc.
+
+    Reference: Phys. Rev. B 45, 13244.
+
+    Args:
+        n: Real-space electronic density.
+        zeta: Relative spin polarization.
+
+    Keyword Args:
+        **kwargs: Throwaway arguments.
+
+    Returns:
+        Modified PW correlation energy density and potential.
+    """
+    # fzeta0 = 4 / (9 * (2**(1 / 3) - 1))
+    return lda_c_pw_spin(
+        n,
+        zeta,
+        A=(0.0310907, 0.01554535, 0.0168869),
+        fzeta0=1.709920934161365617563962776245,
+        **kwargs,
+    )
+
+
 def pbe_x_base(n, dn=None):
   """Base PBE exchange functional to be used in the spin-paired and -polarized case.
 
@@ -51,17 +163,21 @@ def pbe_x_base(n, dn=None):
   mu=0.2195149727645171
 
   breakpoint()
+  if jnp.any(jnp.isnan(dn)):
+      print("NaN exists in dn...")
+      breakpoint()
   norm_dn = jnp.linalg.norm(dn, axis=1)
   kf = (3 * jnp.pi**2 * n) ** (1 / 3)
   # Handle divisions by zero
   # divkf = 1 / kf
   # divkf = jnp.divide(1, kf, out=jnp.zeros_like(kf), where=kf > 0)
-  divkf = jnp.where(kf > 0, 1 / kf, 0)
+  kf_ = jnp.where(kf > 0, kf, 1)
+  divkf = jnp.where(kf > 0, 1 / kf_, 0)
   # Handle divisions by zero
   # s = norm_dn * divkf / (2 * n)
   # s = jnp.divide(norm_dn * divkf, 2 * n, out=jnp.zeros_like(n), where=n > 0)
-#   s = jnp.where(n > 0, norm_dn * divkf / 2 / n, 0)
-  s = norm_dn * divkf / 2 / n
+  n_ = jnp.where(n > 0, n, 1)
+  s = jnp.where(n > 0, norm_dn * divkf / 2 / n_, 0)
   f1 = 1 + mu * s**2 / kappa
   Fx = kappa - kappa / f1
   exunif = -3 * kf / (4 * jnp.pi)
@@ -79,7 +195,8 @@ def pbe_x_base(n, dn=None):
   # vsigmax = jnp.divide(
   #   exunifdFx * divkf, 2 * norm_dn, out=jnp.zeros_like(norm_dn), where=norm_dn > 0
   # )
-  vsigmax = jnp.where(norm_dn > 0, exunifdFx * divkf / 2 / norm_dn, 0)
+  norm_dn_ = jnp.where(norm_dn > 0, norm_dn, 1)
+  vsigmax = jnp.where(norm_dn > 0, exunifdFx * divkf / 2 / norm_dn_, 0)
   return sx * n, jnp.array([vx]), vsigmax
 
 
