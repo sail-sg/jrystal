508a6fa37ad36f34a3ec5305aefb35f4469ba7a6
Zekun Shi <derek.zekun.shi@gmail.com>
2025-04-09 10:35:16 +0800

    debug merge

--- main.py
+++ main.py
@@ -4,49 +4,51 @@ import cloudpickle as pickle
 
 import jrystal as jr
 
-parser = argparse.ArgumentParser(
-  prog='Jrystal', description='Command for Jrystal package.'
-)
 
-parser.add_argument(
-  "-m",
-  "--mode",
-  choices=["energy", "band"],
-  default='energy',
-  help="Set the computation mode. For total enrgy minimization, please use "
-  "\'energy\'. For band structure calculation, please use \'band\'. "
-)
+def main():
+  parser = argparse.ArgumentParser(
+    prog='Jrystal', description='Command for Jrystal package.'
+  )
 
-parser.add_argument(
-  "-c",
-  "--config",
-  default='config.yaml',
-  help="Set the configuration file path."
-)
+  parser.add_argument(
+    "-m",
+    "--mode",
+    choices=["energy", "band"],
+    default='energy',
+    help="Set the computation mode. For total enrgy minimization, please use "
+    "\'energy\'. For band structure calculation, please use \'band\'. "
+  )
 
-parser.add_argument(
-  "-l",
-  "--load",
-  help="Load pickled output from energy calculation for band structure calculation."
-)
+  parser.add_argument(
+    "-c",
+    "--config",
+    default='config.yaml',
+    help="Set the configuration file path."
+  )
 
-args = parser.parse_args()
+  parser.add_argument(
+    "-l",
+    "--load",
+    help="Load pickled output from energy calculation for band structure calculation."
+  )
 
-config = jr.config.get_config("config.yaml")
+  args = parser.parse_args()
 
-if args.mode == "energy":
-  if config.use_pseudopotential:
-    jr.calc.energy_normcons(config)
-  else:
-    jr.calc.energy(config)
-elif args.mode == "band":
+  config = jr.config.get_config("config.yaml")
 
-  gs_output = None
-  if args.load is not None:
-    with open(args.load, 'rb') as f:
-      gs_output = pickle.load(f)
+  if args.mode == "energy":
+    if config.use_pseudopotential:
+      jr.calc.energy_normcons(config)
+    else:
+      jr.calc.energy(config)
+  elif args.mode == "band":
 
-  if config.use_pseudopotential:
-    jr.calc.band_normcons(config, gs_output)
-  else:
-    jr.calc.band(config, gs_output)
+    gs_output = None
+    if args.load is not None:
+      with open(args.load, 'rb') as f:
+        gs_output = pickle.load(f)
+
+    if config.use_pseudopotential:
+      jr.calc.band_normcons(config, gs_output)
+    else:
+      jr.calc.band(config, gs_output)
