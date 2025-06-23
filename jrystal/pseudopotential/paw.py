f461ebeff2bd10fe3b5e4778deb7b413e11bea03
jiaxi98 <jiaxi.zhao@u.nus.edu>
2025-06-23 23:30:20 +0800

    wip

--- jrystal/pseudopotential/paw.py
+++ jrystal/pseudopotential/paw.py
@@ -1,10 +1,21 @@
-from jrystal._src.energy import kinetic
+from jrystal._src.energy import kinetic, hartree, exc_functional
 
 
 g_vector_grid = None
+vol = None
 kpts = None
 coeff_grid = None
 occupation = None
+kohn_sham = True
+
+wave_grid_arr = pw.wave_grid(coefficient, vol)
+
+occupation = jnp.ones(
+  shape=coefficient.shape[:3]
+) if occupation is None else occupation
+
+density_grid = wave_to_density(wave_grid_arr, occupation)
+density_grid_rec = wave_to_density_reciprocal(wave_grid_arr, occupation)
 
 def calculate_pseudo_density(wfs):
   """similar to the wave to density func"""
@@ -49,4 +60,4 @@ def total_energy():
   
    
   # core-valence Hartree energy
-  eh_cv = 
+  eh_cv = hartree(density_grid_rec, g_vector_grid, vol, kohn_sham)
