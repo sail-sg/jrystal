08cece8ffc6ad5bed4a7261e7135b43a294c385e
jiaxi98 <jiaxi.zhao@u.nus.edu>
2025-08-13 23:10:19 +0800

    wip

--- align_paw.py
+++ align_paw.py
@@ -1,61 +1,121 @@
 import jax.numpy as jnp
+import numpy as np
 
+from gpaw.gaunt import gaunt
+from gpaw.new import zips
 from jrystal.pseudopotential.load import parse_upf
 from jrystal._src.energy import kinetic, hartree, exc_functional
 
+def calculate_T_Lqp():
+  Lcut = (2 * lcut + 1)**2
+  G_LLL = gaunt(lcut)[:, :, :Lcut]
+  LGcut = G_LLL.shape[2]
+  T_Lqp = jnp.zeros((Lcut, nq, _np))
+  i = 0
+  j = 0
+  jlL_i = []
+  for l, n in zips(l_j, n_j):
+    for m in range(2 * l + 1):
+      jlL_i.append((j, l, l**2 + m))
+      i += 1
+    j += 1
+  p = 0
+  i1 = 0
+  for j1, l1, L1 in jlL_i:
+    for j2, l2, L2 in jlL_i[i1:]:
+      if j1 < j2:
+        q = j2 + j1 * nj - j1 * (j1 + 1) // 2
+      else:
+        q = j1 + j2 * nj - j2 * (j2 + 1) // 2
+      T_Lqp = T_Lqp.at[:LGcut, q, p].set(G_LLL[L1, L2])
+      p += 1
+    i1 += 1
+  return T_Lqp
+
 pp_dict = parse_upf("/home/aiops/zhaojx/jrystal/pseudopotential/C.pbe-n-kjpaw_psl.1.0.0.UPF")
-lmax = 4   # NOTE: not sure if the n_L is uniform for all atoms
-n_projectors = []
-delta_aiiL = []
-delta0_a = []
-D_aii = []
-n_rgd = 1000
-r_g = pp_dict['PP_MESH']['PP_R']
-dr_g = pp_dict['PP_MESH']['PP_RAB']
+Z = 6
+# Z = int(pp_dict["PP_HEADER"]["Z_valence"])
+r_g = jnp.array(pp_dict['PP_MESH']['PP_R'])
+dr_g = jnp.array(pp_dict['PP_MESH']['PP_RAB'])
+lmax = int(pp_dict["PP_HEADER"]["l_max"])
+l_j = np.array([int(proj['angular_momentum']) for proj in pp_dict['PP_NONLOCAL']['PP_BETA']])
+lcut = max(l_j)
+pt_jg = jnp.array([proj['values'] for proj in pp_dict['PP_NONLOCAL']['PP_BETA']])
+phi_jg = jnp.array([phi['values'] for phi in pp_dict['PP_FULL_WFC']['PP_AEWFC']])
+phit_jg = jnp.array([phi['values'] for phi in pp_dict['PP_FULL_WFC']['PP_PSWFC']])
+nc_g = jnp.array(pp_dict['PP_PAW']['PP_AE_NLCC'])
+nct_g = jnp.array(pp_dict['PP_NLCC'])
 
-nj = n_projectors[atom]
-psi = jnp.zeros((n_rgd, nj))
-psit = jnp.zeros((n_rgd, nj))
-nc_g = jnp.zeros(n_rgd)
-nct_g = jnp.zeros(n_rgd)
-assert r_g.shape[0] == n_rgd
-assert psi.shape[0] == n_rgd
-assert psit.shape[0] == n_rgd
-assert nc_g.shape[0] == n_rgd
-assert nct_g.shape[0] == n_rgd
-D_aii.append(jnp.zeros((nj, nj)))
-delta_aiiL.append(jnp.zeros((nj, nj, (lmax + 1)**2)))
-delta0_a.append(jnp.zeros(1))
-
-"""initialization of delta_aiiL & delta0_a, which does not depend on the 
-pseudized wave function"""
-# nq = nj * (nj + 1) // 2
-# Delta_lq = np.zeros((lmax + 1, nq))
-# for l in range(lmax + 1):
-#   Delta_lq[l] = np.dot(n_qg - nt_qg, r_g**(2 + l) * dr_g)
-# Lmax = (lmax + 1)**2
-# Delta_pL = np.zeros((_np, Lmax))
-# for l in range(lmax + 1):
-#     L = l**2
-#     for m in range(2 * l + 1):
-#         delta_p = np.dot(Delta_lq[l], T_Lqp[L + m])
-#         Delta_pL[:, L + m] = delta_p
-# Delta0 = np.dot(self.local_corr.nc_g - self.local_corr.nct_g,
-#                 r_g**2 * dr_g) - self.Z / sqrt(4 * pi)
-n_qg = (psi[..., None] * psi[:, None]).reshape(-1, nj**2)
-nt_qg = (psit[..., None] * psit[:, None]).reshape(-1, nj**2)
+n_rgd = r_g.shape[0]
+nj = phi_jg.shape[0]
+n_j = np.array([0, 0, 1, 1])
+ni = nj + l_j.sum() * 2
 nq = nj * (nj + 1) // 2
-Delta_lq = jnp.zeros((lmax + 1, nq))
-for l in range(lmax + 1):
-  Delta_lq[l] = jnp.dot(n_qg - nt_qg, r_g**(2 + l) * dr_g)
-Lmax = (lmax + 1)**2
-Delta_pL = jnp.zeros((_np, Lmax))
-for l in range(lmax + 1):
+_np = ni * (ni + 1) // 2
+T_Lqp = calculate_T_Lqp()
+assert r_g.shape[0] == n_rgd
+assert dr_g.shape[0] == n_rgd
+assert phi_jg.shape[1] == n_rgd
+assert phit_jg.shape[1] == n_rgd
+# assert nc_g.shape[0] == n_rgd
+# assert nct_g.shape[0] == n_rgd
+
+# n_projectors = []
+# delta_aiiL = []
+# delta0_a = []
+# D_aii = []
+# nj = n_projectors[atom]
+# nc_g = jnp.zeros(n_rgd)
+# nct_g = jnp.zeros(n_rgd)
+# D_aii.append(jnp.zeros((nj, nj)))
+# delta_aiiL.append(jnp.zeros((nj, nj, (lmax + 1)**2)))
+# delta0_a.append(jnp.zeros(1))
+
+def get_compensation_charges():
+
+  index = jnp.triu_indices(nj)
+  n_qg = (phi_jg[:, None, :] * phi_jg[None])[index]
+  nt_qg = (phit_jg[:, None, :] * phit_jg[None])[index]
+
+  Delta_lq = jnp.zeros((lmax + 1, nq))
+  for l in range(lmax + 1):
+    Delta_lq[l] = jnp.dot(n_qg - nt_qg, r_g**(2 + l) * dr_g)
+
+  Lmax = (lmax + 1)**2
+  Delta_pL = jnp.zeros((_np, Lmax))
+  for l in range(lmax + 1):
     L = l**2
     for m in range(2 * l + 1):
-        delta_p = jnp.dot(Delta_lq[l], T_Lqp[L + m])
-        Delta_pL[:, L + m] = delta_p
-Delta0 = jnp.dot(nc_g - nct_g, r_g**2 * dr_g) - Z / sqrt(4 * pi)
+      delta_p = jnp.dot(Delta_lq[l], T_Lqp[L + m])
+      Delta_pL[:, L + m] = delta_p
+
+  Delta0 = jnp.dot(nc_g - nct_g, r_g**2 * dr_g) - Z / jnp.sqrt(4 * jnp.pi)
+  return (n_qg, nt_qg, Delta_lq, Lmax, Delta_pL, Delta0)
+
+  # g_lg = self.data.create_compensation_charge_functions(lmax)
+  # gcut_q = jnp.zeros(nq, dtype=int)
+  # N0_q = jnp.zeros(nq)
+  # q = 0  # q: common index for j1, j2
+  # for j1 in range(nj):
+  #   for j2 in range(j1, nj):
+  #     gcut = rgd.ceil(min(rcut_j[j1], rcut_j[j2]))
+  #     N0_q[q] = sum(n_qg[q, :gcut] * r_g[:gcut]**2 * dr_g[:gcut])
+  #     gcut_q[q] = gcut
+
+  #     q += 1
+
+  # self.gcut_q = gcut_q
+  # self.N0_q = N0_q
+
+  # # Electron density inside augmentation sphere.  Used for estimating
+  # # atomic magnetic moment:
+  # N0_p = N0_q @ T_Lqp[0] * sqrt(4 * pi)
+
+  # return (g_lg[:, :gcut2].copy(), n_qg, nt_qg,
+  #         Delta_lq, Lmax, Delta_pL, Delta0, N0_p)
+
+breakpoint()
+n_qg, nt_qg, Delta_lq, Lmax, Delta_pL, Delta0 = get_compensation_charges()
 
 """Projectors overlap matrix + overlap correction
 I am not sure whether we should implement the overlap matrix for projectors
@@ -137,10 +197,8 @@ def poisson_rdl(
   return jnp.cumsum(r**l * g_L * dr) / r**l
 
 def four_center_integral(
-  phi1: jnp.ndarray,
-  phi2: jnp.ndarray,
-  phi3: jnp.ndarray,
-  phi4: jnp.ndarray,
+  n1: jnp.ndarray,
+  n2: jnp.ndarray,
   r: jnp.ndarray,
   dr: jnp.ndarray,
 ):
@@ -148,7 +206,7 @@ def four_center_integral(
   Lcut = 25
   result = jnp.zeros((Lcut))
   for l in range(Lcut):
-    result[l] = jnp.sum(phi1 * phi2 * poisson_rdl(phi3 * phi4, r, dr, l))
+    result[l] = jnp.sum(n1 * poisson_rdl(n2, r, dr, l))
 
   M_pp = jnp.zeros((91, 91))
   
