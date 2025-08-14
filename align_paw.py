6cb926854365728509ed7faf30ddd217409d997c
jiaxi98 <jiaxi.zhao@u.nus.edu>
2025-08-14 15:50:15 +0800

    implement M, MB, MB_p

--- align_paw.py
+++ align_paw.py
@@ -1,3 +1,5 @@
+import jax
+jax.config.update("jax_enable_x64", True)
 import jax.numpy as jnp
 import numpy as np
 
@@ -7,6 +9,7 @@ from jrystal.pseudopotential.load import parse_upf
 from jrystal._src.energy import kinetic, hartree, exc_functional
 
 def calculate_T_Lqp():
+  """copied from gpaw"""
   Lcut = (2 * lcut + 1)**2
   G_LLL = gaunt(lcut)[:, :, :Lcut]
   LGcut = G_LLL.shape[2]
@@ -32,44 +35,22 @@ def calculate_T_Lqp():
     i1 += 1
   return T_Lqp
 
-pp_dict = parse_upf("/home/aiops/zhaojx/jrystal/pseudopotential/C.pbe-n-kjpaw_psl.1.0.0.UPF")
-Z = 6
-# Z = int(pp_dict["PP_HEADER"]["Z_valence"])
-r_g = jnp.array(pp_dict['PP_MESH']['PP_R'])
-dr_g = jnp.array(pp_dict['PP_MESH']['PP_RAB'])
-lmax = int(pp_dict["PP_HEADER"]["l_max"])
-l_j = np.array([int(proj['angular_momentum']) for proj in pp_dict['PP_NONLOCAL']['PP_BETA']])
-lcut = max(l_j)
-pt_jg = jnp.array([proj['values'] for proj in pp_dict['PP_NONLOCAL']['PP_BETA']])
-phi_jg = jnp.array([phi['values'] for phi in pp_dict['PP_FULL_WFC']['PP_AEWFC']])
-phit_jg = jnp.array([phi['values'] for phi in pp_dict['PP_FULL_WFC']['PP_PSWFC']])
-nc_g = jnp.array(pp_dict['PP_PAW']['PP_AE_NLCC'])
-nct_g = jnp.array(pp_dict['PP_NLCC'])
-
-n_rgd = r_g.shape[0]
-nj = phi_jg.shape[0]
-n_j = np.array([0, 0, 1, 1])
-ni = nj + l_j.sum() * 2
-nq = nj * (nj + 1) // 2
-_np = ni * (ni + 1) // 2
-T_Lqp = calculate_T_Lqp()
-assert r_g.shape[0] == n_rgd
-assert dr_g.shape[0] == n_rgd
-assert phi_jg.shape[1] == n_rgd
-assert phit_jg.shape[1] == n_rgd
-# assert nc_g.shape[0] == n_rgd
-# assert nct_g.shape[0] == n_rgd
-
-# n_projectors = []
-# delta_aiiL = []
-# delta0_a = []
-# D_aii = []
-# nj = n_projectors[atom]
-# nc_g = jnp.zeros(n_rgd)
-# nct_g = jnp.zeros(n_rgd)
-# D_aii.append(jnp.zeros((nj, nj)))
-# delta_aiiL.append(jnp.zeros((nj, nj, (lmax + 1)**2)))
-# delta0_a.append(jnp.zeros(1))
+def calculate_projector_overlaps():
+  """Compute projector function overlaps B_ii = <pt_i | pt_i>."""
+  B_jj = jnp.sum(pt_jg[:, None, :] * pt_jg[None, :, :] * dr_g, axis=2)
+  breakpoint()
+  B_ii = jnp.zeros((ni, ni))
+  i1 = 0
+  for j1, l1 in enumerate(l_j):
+    for m1 in range(2 * l1 + 1):
+      i2 = 0
+      for j2, l2 in enumerate(l_j):
+        for m2 in range(2 * l2 + 1):
+          if l1 == l2 and m1 == m2:
+            B_ii = B_ii.at[i1, i2].set(B_jj[j1, j2])
+          i2 += 1
+      i1 += 1
+  return B_ii
 
 def get_compensation_charges():
 
@@ -79,17 +60,17 @@ def get_compensation_charges():
 
   Delta_lq = jnp.zeros((lmax + 1, nq))
   for l in range(lmax + 1):
-    Delta_lq[l] = jnp.dot(n_qg - nt_qg, r_g**(2 + l) * dr_g)
+    Delta_lq = Delta_lq.at[l].set(jnp.dot(n_qg - nt_qg, r_g**(2 + l) * dr_g))
 
   Lmax = (lmax + 1)**2
   Delta_pL = jnp.zeros((_np, Lmax))
   for l in range(lmax + 1):
     L = l**2
     for m in range(2 * l + 1):
-      delta_p = jnp.dot(Delta_lq[l], T_Lqp[L + m])
-      Delta_pL[:, L + m] = delta_p
+      Delta_pL = Delta_pL.at[:, L + m].set(jnp.dot(Delta_lq[l], T_Lqp[L + m]))
 
-  Delta0 = jnp.dot(nc_g - nct_g, r_g**2 * dr_g) - Z / jnp.sqrt(4 * jnp.pi)
+  Delta0 = jnp.dot(nc_g - nct_g, r_g**2 * dr_g) * jnp.sqrt(4 * jnp.pi) -\
+    Z / jnp.sqrt(4 * jnp.pi)
   return (n_qg, nt_qg, Delta_lq, Lmax, Delta_pL, Delta0)
 
   # g_lg = self.data.create_compensation_charge_functions(lmax)
@@ -114,32 +95,95 @@ def get_compensation_charges():
   # return (g_lg[:, :gcut2].copy(), n_qg, nt_qg,
   #         Delta_lq, Lmax, Delta_pL, Delta0, N0_p)
 
-breakpoint()
-n_qg, nt_qg, Delta_lq, Lmax, Delta_pL, Delta0 = get_compensation_charges()
-
-"""Projectors overlap matrix + overlap correction
-I am not sure whether we should implement the overlap matrix for projectors
+# load the pseudopotential
+pp_dict = parse_upf("/home/aiops/zhaojx/jrystal/pseudopotential/C.pbe-n-kjpaw_psl.1.0.0.UPF")
+Z = 6
+# Z = int(pp_dict["PP_HEADER"]["Z_valence"])
+r_g = jnp.array(pp_dict['PP_MESH']['PP_R'])
+dr_g = jnp.array(pp_dict['PP_MESH']['PP_RAB'])
+lmax = int(pp_dict["PP_HEADER"]["l_max"])
+l_j = np.array([int(proj['angular_momentum']) for proj in pp_dict['PP_NONLOCAL']['PP_BETA']])
+lcut = max(l_j)
+pt_jg = jnp.array([proj['values'] for proj in pp_dict['PP_NONLOCAL']['PP_BETA']])
+phi_jg = jnp.array([phi['values'] for phi in pp_dict['PP_FULL_WFC']['PP_AEWFC']])
+phit_jg = jnp.array([phi['values'] for phi in pp_dict['PP_FULL_WFC']['PP_PSWFC']])
+r"""
+It is the true charge density, i.e. it will be 
+correctly integrated as \sum_i 4 \pi r_i^2 nlcc_i
+Different from the setting in GPAW, see the discussions
+after (41b)
 """
-def calculate_projector_overlaps(pt_jg):
-  """Compute projector function overlaps B_ii = <pt_i | pt_i>."""
-  nj = len(pt_jg)
-  B_jj = jnp.zeros((nj, nj))
-  for j1, pt1_g in enumerate(pt_jg):
-      for j2, pt2_g in enumerate(pt_jg):
-          B_jj[j1, j2] = rgd.integrate(pt1_g * pt2_g) / (4 * jnp.pi)
-  B_ii = jnp.zeros((ni, ni))
-  i1 = 0
-  for j1, l1 in enumerate(l_j):
-      for m1 in range(2 * l1 + 1):
-          i2 = 0
-          for j2, l2 in enumerate(l_j):
-              for m2 in range(2 * l2 + 1):
-                  if l1 == l2 and m1 == m2:
-                      B_ii[i1, i2] = B_jj[j1, j2]
-                  i2 += 1
-          i1 += 1
-  return B_ii
-B_ii = calculate_projector_overlaps(pt_jg)
+nc_g = jnp.array(pp_dict['PP_PAW']['PP_AE_NLCC'])
+nct_g = jnp.array(pp_dict['PP_NLCC'])
+vbar_g = jnp.array(pp_dict['PP_LOCAL'])
+
+# check the shape of the arrays
+n_rgd = r_g.shape[0]
+nj = phi_jg.shape[0]
+n_j = np.array([0, 0, 1, 1])
+ni = nj + l_j.sum() * 2
+nq = nj * (nj + 1) // 2
+_np = ni * (ni + 1) // 2
+T_Lqp = calculate_T_Lqp()
+assert r_g.shape[0] == n_rgd
+assert dr_g.shape[0] == n_rgd
+assert phi_jg.shape[1] == n_rgd
+assert phit_jg.shape[1] == n_rgd
+assert nc_g.shape[0] == n_rgd
+assert nct_g.shape[0] == n_rgd
+
+n_qg, nt_qg, Delta_lq, Lmax, Delta_pL, Delta0 = get_compensation_charges()
+B_ii = calculate_projector_overlaps()
+
+r_max = jnp.maximum(r_g[None], r_g[:, None])
+
+"""Coulomb related energy corrections"""
+def integrate_radial_function(f_g):
+  return jnp.sum(f_g * r_g**2 * dr_g) * 4 * jnp.pi
+
+def poisson_rdl(
+  g_L: jnp.ndarray,
+  l: int,
+):
+  r"""Solve the Poisson equation over radial grid
+
+  .. math::
+
+  .. warning::
+
+  Args:
+    g_L (Real[Array, "spin kpts band x y z"]): radial function.
+    l (int): angular momentum quantum number.
+
+  Returns:
+    (Real[Array, "spin kpts band x y z"]): radial function.
+  """
+
+  return jnp.sum(r_g**(l + 2) * g_L * dr_g / r_max**l, axis=-1) * 4 * jnp.pi / (2 * l + 1)
+
+A = 0.5 * integrate_radial_function(nc_g * poisson_rdl(nc_g, 0))
+# NOTE: GPAW use jnp.sqrt(4 * jnp.pi) since the n_c is the radial part
+# A -= jnp.sqrt(4 * jnp.pi) * Z * jnp.dot(r_g * dr_g, nc_g)
+A -= 4 * jnp.pi * Z * jnp.dot(r_g * dr_g, nc_g)
+mct_g = nct_g + Delta0 * g_lg[0]
+A -= 0.5 * integrate_radial_function(mct_g * poisson_rdl(mct_g, 0))
+M = A
+MB = -integrate_radial_function(nct_g * vbar_g)
+
+AB_q = -integrate_radial_function(nt_qg * vbar_g)
+MB_p = jnp.dot(AB_q, T_Lqp[0])
+
+
+def four_center_integral(
+  n1: jnp.ndarray,
+  n2: jnp.ndarray,
+):
+  
+  Lcut = 25
+  result = jnp.zeros((Lcut))
+  for l in range(Lcut):
+    result[l] = jnp.sum(n1 * poisson_rdl(n2, r_g, dr_g, l)) 
+
 
 for i in range(n_projectors[atom]):
   for j in range(n_projectors[atom]):
@@ -170,46 +214,7 @@ def calculate_atomic_density_matrices(D_asp):
 
 def calculate_multipole_moments():
 
-  return comp_charge, _Q_aL
-
-def poisson_rdl(
-  g_L: jnp.ndarray,
-  r: jnp.ndarray,
-  dr: jnp.ndarray,
-  l: int,
-):
-  r"""Solve the Poisson equation over radial grid
-
-  .. math::
-
-  .. warning::
-
-  Args:
-    g_L (Real[Array, "spin kpts band x y z"]): radial function.
-    r (Real[Array, "spin kpts band x y z"]): radial grid.
-    dr (Real[Array, "spin kpts band x y z"]): radial grid spacing.
-    l (int): angular momentum quantum number.
-
-  Returns:
-    (Real[Array, "spin kpts band x y z"]): radial function.
-  """
-
-  return jnp.cumsum(r**l * g_L * dr) / r**l
-
-def four_center_integral(
-  n1: jnp.ndarray,
-  n2: jnp.ndarray,
-  r: jnp.ndarray,
-  dr: jnp.ndarray,
-):
-  
-  Lcut = 25
-  result = jnp.zeros((Lcut))
-  for l in range(Lcut):
-    result[l] = jnp.sum(n1 * poisson_rdl(n2, r, dr, l))
-
-  M_pp = jnp.zeros((91, 91))
-  
+  return comp_charge, _Q_aL 
 
 def correction():
   correction = 0
@@ -219,6 +224,13 @@ def correction():
     )
     
   return correction
+
+# delta_aiiL = []
+# delta0_a = []
+# D_aii = []
+# D_aii.append(jnp.zeros((nj, nj)))
+# delta_aiiL.append(jnp.zeros((nj, nj, (lmax + 1)**2)))
+# delta0_a.append(jnp.zeros(1))
 
 def total_energy():
 
