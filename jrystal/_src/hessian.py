8df74a6dc8747de257ae063925f9f137e067378d
nicktianboli <li_tianbo@live.com>
2025-10-06 18:38:28 +0800

    add hessian_diag

--- jrystal/_src/hessian.py
+++ jrystal/_src/hessian.py
@@ -13,6 +13,8 @@
 # limitations under the License.
 """Hessian for Complex-Valued Functions. """
 from typing import Callable
+import operator
+import numpy as np
 
 import jax
 import jax.numpy as jnp
@@ -29,7 +31,8 @@ def complex_hessian(
     Only for :math:`x` being a vector is tested.
 
   Args:
-    f: A function that takes a complex-valued array and returns a complex-valued array.
+    f: A function that takes a complex-valued array and returns a
+    complex-valued array.
     primal: The point at which to compute the Hessian.
 
   Returns:
@@ -46,10 +49,118 @@ def complex_hessian(
     _, vjp_fn = jax.vjp(grad_f, x)
     return vjp_fn(v)[0]
 
-  # x = jnp.ones(dim, dtype=dtype)
   return jnp.array(
     [
       hessian_vec_prod(primal, jnp.eye(dim, dtype=dtype)[i])
       for i in range(dim)
     ]
   )
+
+
+def hvp(f, x, v):
+  """compute the the Hessian-vector product.
+
+  Args:
+    f: A function that takes a complex-valued vector and returns a
+    real-valued scalar.
+    x: The primal at which to compute the Hessian-vector product.
+    v: The vector at which to compute the Hessian-vector product.  It must have
+    the same structure as the output of f.
+
+  Returns:
+    The Hessian-vector product of the function .
+  """
+
+  _, vjp_fun = jax.vjp(jax.grad(f), x)
+
+  # the vjp_fun takes an object that has the same shape as the output of the
+  # function f, same structure if the return is a pytree.
+
+  return vjp_fun(v)[0].T.conj()  # here assume that the output is a vector.
+
+
+def hessian_diag_vector(f: Callable) -> Callable:
+  """Compute the diagonal elements of the Hessian matrix of a function
+  evalueated at the primal value x.
+
+  This function only applies to the case where the input is a vector.
+
+  Args:
+    f (Callable): A function that takes a vector and returns a real-valued
+    scalar.
+
+  Returns:
+    Callable: A function that takes a vector and returns the diagonal elements
+    of the Hessian matrix of the function at the point.
+  """
+
+  def _hess_diag_vec(x):
+    n = x.shape[0]
+    # basis = jnp.eye(n, dtype=x.dtype)
+
+    def g(carry, xx):
+      e = jnp.zeros(n, dtype=x.dtype)
+      e = e.at[xx].set(1.).astype(x.dtype)
+      # Each diag entry is e_i^T H e_i
+      y = hvp(f, x, e).dot(e)     # trade time for space.
+      return None, y
+
+    return jax.lax.scan(g, None, jnp.arange(n))[1]
+
+  return _hess_diag_vec
+
+
+def hessian_diag_pytree(f: Callable) -> Callable:
+  """Compute the diagonal elements of the Hessian matrix of a function.
+
+  Args:
+    f (Callable): A function that takes a PyTree and returns a real-valued
+    scalar.
+
+  Returns:
+    Callable:A function that takes a PyTree and returns the diagonal elements
+    of the Hessian matrix of the function.
+  """
+  grad_fn = jax.grad(f)
+
+  def hvp(x, v):
+    v = jax.tree.map(lambda a, b: b.astype(a.dtype), x, v)
+    """Hessian-vector product"""
+    return jax.jvp(grad_fn, (x,), (v,))[1]
+    # the output and input of grad_fn have same structure, therefore jvp and
+    # vjp are equally effective.
+
+  def _diag_hessian(x):
+    flat_x, tree_def = jax.tree_util.tree_flatten(x)
+
+    result = []
+    dtype = jnp.int32 if flat_x[0].dtype == jnp.complex64 else jnp.int64
+    flat_x_zeros = jax.tree.map(
+      lambda x: jnp.zeros(x.shape, dtype=dtype), flat_x
+    )
+
+    for leaf_idx, leaf in enumerate(flat_x):
+      leaf_zeros = jnp.zeros(leaf.shape, dtype=leaf.dtype)
+
+      def f_scan(carry, idx):
+        _result, leaf, flat_x_zeros = carry
+        mask = jnp.zeros(leaf.shape, dtype=jnp.bool)
+        mask = mask.at[idx].set(True)
+        e = flat_x_zeros.copy()
+        e[leaf_idx] = mask
+        e = jax.tree.unflatten(tree_def, e)
+        ehv = hvp(x, e)
+        ehve = jax.tree.map(lambda x, y: x * y, ehv, e)
+        _result = _result + jax.tree.flatten(ehve)[0][leaf_idx]
+
+        return (_result, leaf, flat_x_zeros), None
+
+      # breakpoint()
+      scan_carry, _ = jax.lax.scan(
+        f_scan, (leaf_zeros, leaf, flat_x_zeros),
+        np.array([idx for idx in np.ndindex(leaf.shape)])
+      )
+      result.append(scan_carry[0])
+
+    return jax.tree.unflatten(tree_def, result)
+  return _diag_hessian
--- jrystal/_src/hessian_test.py
+++ jrystal/_src/hessian_test.py
@@ -16,7 +16,9 @@ import jax
 import jax.numpy as jnp
 import numpy as np
 from absl.testing import absltest, parameterized
-from jrystal._src.hessian import complex_hessian
+from jrystal._src.hessian import (
+  complex_hessian, hvp, hessian_diag_pytree, hessian_diag_vector
+)
 
 jax.config.update("jax_enable_x64", True)
 
@@ -24,7 +26,7 @@ jax.config.update("jax_enable_x64", True)
 class _TestHessian(parameterized.TestCase):
 
   def setUp(self):
-    self.key = jax.random.PRNGKey(123)
+    self.key = jax.random.PRNGKey(12341)
 
   def test_quadratic_form(self):
     dim = 10
@@ -43,6 +45,56 @@ class _TestHessian(parameterized.TestCase):
     # print(hessian_matrix)
     # print(H)
     np.testing.assert_allclose(hessian_matrix, H)
+
+  def test_hvp(self):
+    dim = 10
+    key1, key2 = jax.random.split(self.key, 2)
+    a = jax.random.normal(key1, shape=(10, 10))
+    b = jax.random.normal(key2, shape=(10, 10))
+    v = jax.random.uniform(key2, shape=(10)) + 1.j
+
+    A = a + b * 1.j
+    H = A + A.conj().T
+
+    def f(x):
+      return jnp.real(1 / 2 * x.conj().T @ H @ x)
+
+    x = jnp.ones(dim, dtype=A.dtype)
+
+    hv = hvp(f, x, v)
+    np.testing.assert_allclose(hv, H@v)
+
+  def test_hessian_diag_pytree(self):
+    dim = 10
+    key1, key2 = jax.random.split(self.key, 2)
+    a = jax.random.normal(key1, shape=(10, 10))
+    b = jax.random.normal(key2, shape=(10, 10))
+
+    A = a + b * 1.j
+    H = A + A.conj().T
+
+    def f(x):
+      return jnp.real(1. / 2 * x.conj().T @ H @ x)
+
+    x = jnp.ones(dim, dtype=A.dtype)
+    hdiag = hessian_diag_pytree(f)(x)
+    np.testing.assert_allclose(hdiag, jnp.diag(H))
+
+  def test_hessian_diag_vector(self):
+    dim = 10
+    key1, key2 = jax.random.split(self.key, 2)
+    a = jax.random.normal(key1, shape=(10, 10))
+    b = jax.random.normal(key2, shape=(10, 10))
+
+    A = a + b * 1.j
+    H = A + A.conj().T
+
+    def f(x):
+      return jnp.real(1 / 2 * x.conj().T @ H @ x)
+
+    x = jnp.ones(dim, dtype=A.dtype)
+    hdiag = hessian_diag_vector(f)(x)
+    np.testing.assert_allclose(hdiag, jnp.diag(H))
 
 
 if __name__ == '__main__':
