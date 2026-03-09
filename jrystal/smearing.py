c236089285f5b520d5da4803369a4961ca95ccc5
nicktianboli <li_tianbo@live.com>
2026-03-09 11:06:07 +0800

    add smearing

---
+++ jrystal/smearing.py
@@ -0,0 +1,15 @@
+from jrystal._src.smearing import (
+  fermi_dirac,
+  find_chemical_potential,
+  gaussian,
+  marzari_vanderbilt,
+  methfessel_paxton,
+)
+
+__all__ = [
+  "fermi_dirac",
+  "gaussian",
+  "marzari_vanderbilt",
+  "methfessel_paxton",
+  "find_chemical_potential",
+]
